92f6f2ce354a0682030947e5d3822c775a4ac6db
C-cld <meetcaiyy@outlook.com>
2019-06-01 22:57:22 +0800

    体重设置

--- src/main/java/com/cyy/controller/HomeController.java
+++ src/main/java/com/cyy/controller/HomeController.java
@@ -1,23 +1,15 @@
 package com.cyy.controller;
 
-import java.text.ParseException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.cyy.domain.Weight;
-import com.cyy.service.CategoryService;
 import com.cyy.service.WeightService;
 
 @Controller
 public class HomeController 
 {
-	@Autowired
-	CategoryService categoryService;
 	@Autowired
 	WeightService weightService;
 	
@@ -43,8 +35,6 @@ public class HomeController
 		try 
 		{
 			ModelAndView mav=new ModelAndView("index");
-			List<Weight> weightList = weightService.find();
-			mav.addObject("weightList", weightList);
 			return mav;
 		} 
 		catch (Exception e) 
@@ -54,17 +44,5 @@ public class HomeController
 		}
 	}
 	
-	@RequestMapping(value = "/add")
-	public ModelAndView add(@RequestParam(value = "date", required = true) String date, 
-							@RequestParam(value = "amWeight", required = true, defaultValue = "0") String amWeight, 
-							@RequestParam(value = "pmWeight", required = true, defaultValue = "0") String pmWeight) {
-		try {
-			weightService.add(date, amWeight, pmWeight);
-		} catch (ParseException e) {
-			// 日期转换异常
-			e.printStackTrace();
-		}
-		ModelAndView mav=new ModelAndView("redirect:/index");
-		return mav;
-	}
+	
 }
---
+++ src/main/java/com/cyy/controller/WeightController.java
@@ -0,0 +1,57 @@
+package com.cyy.controller;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cyy.domain.Weight;
+import com.cyy.service.WeightService;
+
+@Controller
+public class WeightController {
+	
+	@Autowired
+	WeightService weightService;
+	
+	@RequestMapping(value = "/weight")
+	public ModelAndView weight() 
+	{
+		try 
+		{
+			ModelAndView mav=new ModelAndView("weight");
+			List<Weight> weightList = weightService.find();
+			mav.addObject("weightList", weightList);
+			return mav;
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@RequestMapping(value = "/add")
+	public ModelAndView add(@RequestParam(value = "date", required = true) String date, 
+							@RequestParam(value = "amWeight", required = true, defaultValue = "0") String amWeight, 
+							@RequestParam(value = "pmWeight", required = true, defaultValue = "0") String pmWeight) {
+		try {
+			weightService.add(date, amWeight, pmWeight);
+		} catch (ParseException e) {
+			// 日期转换异常
+			e.printStackTrace();
+		}
+		ModelAndView mav=new ModelAndView("redirect:/weight");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/weight-setting")
+	public ModelAndView setting() {
+		ModelAndView mav = new ModelAndView("weight-setting");
+		return mav;
+	}
+}
