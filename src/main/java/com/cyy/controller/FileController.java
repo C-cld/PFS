325667492426d0d6eff06867c84d5abfb649f421
C-cld <meetcaiyy@outlook.com>
2019-08-10 22:38:56 +0800

    视频上传和在线播放文件

--- src/main/java/com/cyy/controller/FileController.java
+++ src/main/java/com/cyy/controller/FileController.java
@@ -1,8 +1,18 @@
 package com.cyy.controller;
 
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.UUID;
 
 @Controller
 public class FileController {
@@ -10,5 +20,54 @@ public class FileController {
     public ModelAndView porn() {
         ModelAndView mav = new ModelAndView("porn");
         return mav;
+    }
+
+    @RequestMapping(value = "/upload-file", method = RequestMethod.POST)
+    @ResponseBody
+    public String upload(@RequestParam("videoFile") MultipartFile uploadfile, @RequestParam("videoTags") String tagIds) {
+        //String fileName = uploadfile.getOriginalFilename();
+        String fileName = UUID.randomUUID().toString().replace("-", "").toLowerCase();
+        String filePath = "E://";
+        File dest = new File(filePath + fileName);
+        try {
+            uploadfile.transferTo(dest);
+            return "上传成功";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败！";
+    }
+
+    @RequestMapping(value = "/play-video")
+    @ResponseBody
+    public void play(HttpServletRequest request , HttpServletResponse response) {
+
+        File file = new File("E://uploadvideo/7115a6d9e67e4ef5b0468f23bedfe46f");
+        FileInputStream input = null;
+        OutputStream output = null;
+        try {
+            input = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int i = 0;
+        try {
+            i = input.available();
+            byte[] bytes = new byte[i];
+            input.read(bytes);
+            response.setContentType("application/video");
+            output = response.getOutputStream();
+            output.write(bytes);
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                output.close();
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/resources/static/js/porn.js
+++ src/main/resources/static/js/porn.js
@@ -1,3 +1,18 @@
+// 选中标签
+var selectedTag = [];
+resizeTagAndFile();
+$(window).resize(function () {
+    resizeTagAndFile();
+});
+
+function resizeTagAndFile() {
+    var bodyHeight = $("body").height();
+    var tagHeight = $(".tag").height();
+    var fileHeight = bodyHeight - tagHeight;
+    /*$(".file").height(bodyHeight - tagHeight);*/
+    $(".files").css({"height": fileHeight + "px", "margin-top" : tagHeight + "px"});
+}
+
 // 标签库里的标签添加点击事件
 $(".label-items").on("click", "li", function(){
     var id = $(this).attr("data");
@@ -7,10 +22,45 @@ $(".label-items").on("click", "li", function(){
     }
     if (addLabel(id, text)) {
         $(this).addClass("selected");
+        selectedTag.push(id);
     }
+    // 查询选中标签
 });
 // 添加标签到顶端区域
 function addLabel(id,text) {
     var labelHTML = "<li data='"+id+"''>"+text+"<div class='delete'></div></li>";
     $(".label-selected").append(labelHTML);
+    return true;
 }
+// 删除标签
+$(".label-selected").on("click", "li .delete", function(){
+    var id = $(this).parent().attr("data");
+    // 从选中标签中移除
+    $(this).parent().remove();
+    // 还原标签库中选中标签的颜色
+    $(".label-items").find("li[data='"+id+"']").removeClass("selected");
+});
+
+// 上传文件
+$(".upload").on("click", function(){
+    var formData = new FormData();
+    formData.append("videoFile", $(".file")[0].files[0]);
+    formData.append("videoTags", selectedTag);
+    $.ajax({
+        url:"./upload-file",
+        type:"POST",
+        data: formData,
+        enctype: 'multipart/form-data',
+        cache: false,
+        processData:false, // 使数据不做处理
+        contentType:false, // 不要设置Content-Type请求头
+        success:function(data){
+            if (data == 'success') {
+                alert('上传成功！');
+            }
+        },
+        error:function(request) {
+            alert("上传文件失败 ");
+        }
+    });
+});
