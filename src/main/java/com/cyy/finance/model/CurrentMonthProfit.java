52214669aae4cd07ae99b6a9879123b1030a0953
caiyunye <meetcaiyy@outlook.com>
2020-03-12 14:36:55 +0800

    下拉菜单

--- src/main/java/com/cyy/finance/model/CurrentMonthProfit.java
+++ src/main/java/com/cyy/finance/model/CurrentMonthProfit.java
@@ -1,8 +1,17 @@
 package com.cyy.finance.model;
 
 public class CurrentMonthProfit {
+    private Integer id;
     private String name;
     private Float value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
